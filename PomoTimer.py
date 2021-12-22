176a83525811750ef98825cb1582635d9aa16e81
Mark <72723296+markl12s@users.noreply.github.com>
2021-12-22 15:34:27 -0600

    Create PomoTimer.py

---
+++ PomoTimer.py
@@ -0,0 +1,44 @@
+"""
+PomoTimer
+Created by Mark
+
+designed as a Pomodoro Timer system for doing schoolwork
+
+sleep time in seconds
+"""
+import typer
+import time
+app = typer.Typer()
+
+"""
+---------------------------------------------------------------------------------------------------------
+functions
+---------------------------------------------------------------------------------------------------------
+"""
+
+@app.command()
+def timer(on_time: str, off_time: str, iterations: str, turn_on='playerctl play', turn_off='playerctl pause'):
+    on_time = float(on_time)
+    off_time = float(off_time)
+    iterations = int(iterations)
+
+    for i in range(iterations):
+        print(turn_on)
+        # loading bar
+        time.sleep(on_time)
+        print(turn_off)
+        # loading bar
+        time.sleep(off_time)
+
+@app.command()
+def setup():
+    print('sudo apt install playerctl')
+
+"""
+---------------------------------------------------------------------------------------------------------
+execution
+---------------------------------------------------------------------------------------------------------
+"""
+
+if __name__ == "__main__":
+    app()
