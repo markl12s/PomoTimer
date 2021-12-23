3ed6c8961b25a79258e7898bb57756f77cfa3bcc
Mark <72723296+markl12s@users.noreply.github.com>
2021-12-23 10:47:13 -0600

    Update PomoTimer.py

--- PomoTimer.py
+++ PomoTimer.py
@@ -3,10 +3,36 @@ PomoTimer
 
 designed as a Pomodoro Timer system for doing schoolwork
 """
-
 import typer
 import time
 app = typer.Typer()
+
+"""
+---------------------------------------------------------------------------------------------------------
+terminal commands
+---------------------------------------------------------------------------------------------------------
+"""
+
+@app.command()
+def timer(on_time: str, off_time: str, iterations: str, turn_on='playerctl play', turn_off='playerctl pause'):
+    on_time_arr = on_time.split(':')
+    off_time_arr = off_time.split(':')
+
+    iterations = int(iterations)
+
+    on_time_tot = find_time_tot(on_time_arr)
+    off_time_tot = find_time_tot(off_time_arr)
+
+    for i in range(iterations):
+        print(turn_on)
+        time.sleep(on_time_tot)
+
+        print(turn_off)
+        time.sleep(off_time_tot)
+
+@app.command()
+def setup():
+    print('sudo apt install playerctl')
 
 """
 ---------------------------------------------------------------------------------------------------------
@@ -14,22 +40,19 @@ functions
 ---------------------------------------------------------------------------------------------------------
 """
 
-@app.command()
-def timer(on_time: str, off_time: str, iterations: str, turn_on='playerctl play', turn_off='playerctl pause'):
-    on_time = float(on_time)
-    off_time = float(off_time)
-    iterations = int(iterations)
+def find_time_tot(time):
+    iterations = len(time)
+    multiplier = 1
+    multiply = [1]
+
+    for i in range(iterations): multiply.append(multiplier * 60)
 
     for i in range(iterations):
-        print(turn_on)
-        time.sleep(on_time)
-        
-        print(turn_off)
-        time.sleep(off_time)
+        value = -abs(i)
+        final_value = int(time[value]) * multiply[i]
 
-@app.command()
-def setup():
-    print('sudo apt install playerctl')
+    return final_value
+
 
 """
 ---------------------------------------------------------------------------------------------------------
