04a1ce7a42e4b72a6f3f02a1bccd64bc2ab9014d
Mark <72723296+markl12s@users.noreply.github.com>
2021-12-31 13:25:43 -0600

    rolled back to last stable version

--- PomoTimer.py
+++ PomoTimer.py
@@ -1,6 +1,5 @@
 """
 PomoTimer
-
 designed as a Pomodoro Timer system for doing schoolwork
 """
 import typer
@@ -15,9 +14,8 @@ terminal commands
 ---------------------------------------------------------------------------------------------------------
 """
 
-#figure out how to kill cmatrix
 @app.command()
-def timer(on_time: str, off_time: str, iterations: str, turn_on_actions=['kill cmatrix', 'playerctl play'], turn_off_actions=['playerctl pause', 'cmatrix'], end_timer='nothing'):
+def timer(on_time: str, off_time: str, iterations: str, turn_on='playerctl play', turn_off='playerctl pause', end_timer='nothing'):
     on_time_arr = on_time.split(':')
     off_time_arr = off_time.split(':')
 
@@ -27,21 +25,21 @@ def timer(on_time: str, off_time: str, iterations: str, turn_on_actions=['kill c
     off_time_tot = find_time_tot(off_time_arr)
 
     for i in range(iterations - 1):
-        turn_on(turn_on_actions, on_time_tot)
+        os.system(turn_on)
+        time.sleep(on_time_tot)
 
-        turn_off(turn_off_actions, off_time_tot)
+        os.system(turn_off)
+        time.sleep(off_time_tot)
 
     os.system(turn_on)
     time.sleep(on_time_tot)
 
     if end_timer != 'nothing':
-        if end_timer == 'repeat':
-            turn_off_action(turn_off_actions, off_time_tot)
+        os.system(end_timer)
 
 @app.command()
 def setup():
     os.system('sudo apt install playerctl')
-    os.system('sudo apt install cmatrix')
 
 """
 ---------------------------------------------------------------------------------------------------------
@@ -61,19 +59,6 @@ def find_time_tot(time):
         final_value = int(time[value]) * multiply[i]
 
     return final_value
-
-def turn_on(turn_on_action, on_time_tot):
-    for i in range(len(turn_on_action)):
-        os.system(turn_on_action[i])
-
-    time.sleep(on_time_tot)
-
-def turn_off(turn_off_action, off_time_tot):
-    for i in range(len(turn_off_action)):
-        os.system(turn_off_action[i])
-
-    time.sleep(off_time_tot)
-
 
 """
 ---------------------------------------------------------------------------------------------------------
